e27611c402a6204fe81aa9983558eceb03300c55
Vikasboharpi <vikasboharpee74@gmail.com>
2026-01-07 15:25:45 +0530

    changes

--- AssetManagement.API/Program.cs
+++ AssetManagement.API/Program.cs
@@ -117,42 +117,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+app.Run();
 
-// Sample endpoint (can be removed later)
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
-try
-{
-    Log.Information("Starting AssetManagement API");
-    app.Run();
-}
-catch (Exception ex)
-{
-    Log.Fatal(ex, "Application terminated unexpectedly");
-}
-finally
-{
-    Log.CloseAndFlush();
-}
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- AssetManagement.API/obj/Debug/net10.0/AssetManagement.API.AssemblyInfo.cs
+++ AssetManagement.API/obj/Debug/net10.0/AssetManagement.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AssetManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d82e82d1d43c3733109cd1e01458d4a96041f598")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d4044594943f856c1402a858b6ca98ab27f286")]
 [assembly: System.Reflection.AssemblyProductAttribute("AssetManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AssetManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
