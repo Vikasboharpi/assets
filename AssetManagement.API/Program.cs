55e7daab5199446636bab4b706263d215231f32a
Vikasboharpi <vikasboharpee74@gmail.com>
2026-01-13 18:09:39 +0530

    cd

--- AssetManagement.API/Program.cs
+++ AssetManagement.API/Program.cs
@@ -152,30 +152,6 @@ if (builder.Environment.IsDevelopment())
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Asset Management API V1");
-        c.RoutePrefix = "swagger";
-        c.DocumentTitle = "Asset Management API Documentation";
-        c.DefaultModelsExpandDepth(-1); // Hide schemas section by default
-        c.DisplayRequestDuration();
-        c.EnableDeepLinking();
-        c.EnableFilter();
-        c.ShowExtensions();
-        c.EnableValidator();
-        
-        // Custom CSS for better appearance
-        c.InjectStylesheet("/swagger-ui/custom.css");
-        
-        // Add custom JavaScript for better UX
-        c.InjectJavascript("/swagger-ui/custom.js");
-    });
-}
-else
-{
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
@@ -195,7 +171,7 @@ else
         // Add custom JavaScript for better UX
         c.InjectJavascript("/swagger-ui/custom.js");
     });
-}
+
 
 // Enable static files for custom Swagger assets
 app.UseStaticFiles();
